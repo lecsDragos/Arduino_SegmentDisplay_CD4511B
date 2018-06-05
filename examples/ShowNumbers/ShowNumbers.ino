c6d4a81fe43982d28d06d542c52ae396a4ebd7f8
Alex Stanciu <>
2018-06-05 22:43:06 +0300

    added comments to the example

--- examples/ShowNumbers/ShowNumbers.ino
+++ examples/ShowNumbers/ShowNumbers.ino
@@ -1,18 +1,27 @@
 #include <SegmentDisplay_CD4511B.h>
 
-int numberToDisplay = 0;
+// the number that will get displayed
+int number = 0;
 
-SegmentDisplay_CD4511B sg(6,8,7,5,2,9,10);
+/*
+ * The parameters of the SegmentDisplay_CD4511B constructor:
+ *    First 4 numbers are the Arduino pins connected to the  A, B, C, D pins of the CD4511B chip(s)
+ *      in this example 6,8,7,5 are the pins used
+ *    5-th number is the number of chips used 
+ *      in this example 2 chips are used to drive 2 displays
+ *    the remaining pins are the Arduino pins connected to the latching pins of the CD4511B chip(s) 
+ *      in this example 9 and 10 are the numbers of the latching pins
+*/
+SegmentDisplay_CD4511B displayDriver(6,8,7,5,2,9,10);
 
-void setup() {
-}
+void setup() {}
 
 void loop() {
-  if (numberToDisplay > 12) { numberToDisplay = 0; }
+  if (number > 20) { number = 0; }
   
-  sg.displayNumber(numberToDisplay);
+  displayDriver.displayNumber(number);
   
-  numberToDisplay++;
+  number++;
   
   delay(250);  
 }
